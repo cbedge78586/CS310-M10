0a11a4bda7df641183af1e85b29e1ea23085f584
Charles Edge <cbedge@my365.bellevue.edu>
2025-11-11 08:03:44 -0600

    Add files via upload

---
+++ M10 Discussion.cpp
@@ -0,0 +1,24 @@
+﻿// Charles Edge
+// CS310-T301
+// M10: Discussion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// Init as an integer variable
+    int number = 10;
+
+// Declare pointer as an integer
+    int* ptr;
+
+    *ptr = number;          
+
+// Output to screen  
+    cout << "The value is: " << *ptr << endl;
+    double* dptr = &number;  
+
+// End program
+    return 0;
+}
